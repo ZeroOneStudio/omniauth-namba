d386e5843902e9a34734a353c8751abbcf60e00e
Sergey Kishenin <sergey.kishenin@gmail.com>
2012-08-23 13:07:52 +0600

    some tests added

--- spec/omniauth/strategies/namba_spec.rb
+++ spec/omniauth/strategies/namba_spec.rb
@@ -8,4 +8,20 @@ describe "OmniAuth Namba" do
   it 'should add a camelization for itself' do
     OmniAuth::Utils.camelize('namba').should == 'Namba'
   end
+
+  it 'should has correct Namba site' do
+    subject.options.client_options.site.should eq('http://api.namba.kg')
+  end
+
+  it 'should has correct request token path' do
+    subject.options.client_options.request_token_path.should eq('/oauth/request_token.php')
+  end
+
+  it 'should has correct access token path' do
+    subject.options.client_options.access_token_path.should eq('/oauth/access_token.php')
+  end
+
+  it 'should has correct authorize url' do
+    subject.options.client_options.authorize_url.should eq('http://login.namba.kg/login2.php')
+  end
 end
